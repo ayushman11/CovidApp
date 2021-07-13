dde82d4873a7d1014a3d6d4e99b114f2fb35e60b
Ayushman <ayushman11022002@gmail.com>
2021-07-14 04:32:01 +0530

    add India datagit

--- CovidApp/try.py
+++ CovidApp/try.py
@@ -1,16 +1,33 @@
+from os import error
 import requests
-text= requests.get('https://www.worldometers.info/coronavirus/').text
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+LANGUAGE = "en-US,en;q=0.5"
+session = requests.Session()
+session.headers['User-Agent'] = USER_AGENT
+session.headers['Accept-Language'] = LANGUAGE
+session.headers['Content-Language'] = LANGUAGE
+text= session.get('https://www.google.com/search?q=Coronavirus+delhi&biw=1366&bih=625&tbm=nws&sxsrf=ALeKk02EjSFMS4VAFdmugQbCo6PXEPWIYg%3A1626179779235&ei=w4jtYN3cDd_B3LUPxo-GaA&oq=Coronavirus+india&gs_l=psy-ab.3..0i433i131k1j0i433k1l2j0i433i131k1j0i433k1j0i433i131k1j0i433k1j0i433i131k1l2j0i433k1.112257.113775.0.114002.10.9.0.0.0.0.260.1002.0j2j3.5.0....0...1c.1.64.psy-ab..6.4.859...0j0i433i67k1j0i67k1j0i433i131i273k1j0i273k1.0.uaiWtMRTDTI').text
+
 
 from bs4 import BeautifulSoup
-html_text= text
-soup= BeautifulSoup(html_text, 'html.parser')
-data= soup.find_all('div', {'id': 'maincounter-wrap'})
-world_info=[]
+soup= BeautifulSoup(text, 'html5lib')
+articles= soup.find_all('div', class_="kCrYT")
+news= []
 
-for x in data:
-    world_info.append(int(x.div.span.text.replace(',','')))
+# print(articles)
 
-world_info.insert(1,world_info[0]-world_info[2])
+for article in articles:
+    try:
+        url_temp= article.a['href']
+        url= (url_temp.split("/url?q=")[1]).split("&sa=U&ved")[0]
+        newspaperx= article.find('div', class_='BNeawe UPmit AP7Wnd').text
+        # newspaper= newspaperx.split('<div class="BNeawe UPmit AP7Wnd">')[1].split('</div>')[0]
 
-for i in range(4):
-    world_info[i]='{:,}'.format(world_info[i])
+        headline= article.find('div', class_='vvjwJb').text
+        # print(headline)   
+        news.append([newspaperx, headline, url])
+    except AttributeError:
+        continue
+
+
+print(news)
--- CovidApp/views.py
+++ CovidApp/views.py
@@ -4,6 +4,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def html_content_news_india():
+    import requests
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    text= session.get(f'https://www.google.com/search?q=Coronavirus+India&biw=1366&bih=625&tbm=nws&sxsrf=ALeKk02EjSFMS4VAFdmugQbCo6PXEPWIYg%3A1626179779235&ei=w4jtYN3cDd_B3LUPxo-GaA&oq=Coronavirus+india&gs_l=psy-ab.3..0i433i131k1j0i433k1l2j0i433i131k1j0i433k1j0i433i131k1j0i433k1j0i433i131k1l2j0i433k1.112257.113775.0.114002.10.9.0.0.0.0.260.1002.0j2j3.5.0....0...1c.1.64.psy-ab..6.4.859...0j0i433i67k1j0i67k1j0i433i131i273k1j0i273k1.0.uaiWtMRTDTI').text
+
+    return text
+
+
 def html_content_world():
     import requests
     text= requests.get('https://www.worldometers.info/coronavirus/').text
@@ -24,9 +37,29 @@ def html_content_india():
     return text
 
 def india_headline():
+
     from bs4 import BeautifulSoup
-    html_text= html_content_india()
-    soup= BeautifulSoup(html_text, 'html.parser')
+    soup= BeautifulSoup(html_content_news_india(), 'html5lib')
+    articles= soup.find_all('div', class_="kCrYT")
+    news= []
+
+    # print(articles)
+
+    for article in articles:
+        try:
+            url_temp= article.a['href']
+            url= (url_temp.split("/url?q=")[1]).split("&sa=U&ved")[0]
+            newspaper= article.find('div', class_='BNeawe UPmit AP7Wnd').text
+            # newspaper= newspaperx.split('<div class="BNeawe UPmit AP7Wnd">')[1].split('</div>')[0]
+
+            headline= article.find('div', class_='vvjwJb').text
+            # print(headline)   
+            news.append([newspaper, headline, url])
+        except AttributeError:
+            continue
+
+    # news_final= news[:3]
+    return news
 
 def india_data():
     from bs4 import BeautifulSoup
@@ -104,8 +137,10 @@ def index(request):
     worldwide= worldwide_data()
     world_data= country_cases()
     india= india_data()
+    headlines= india_headline()
     country_data= json.dumps(world_data)
     return render(request, 'CovidApp/index.html', {
+        'headlines': headlines,
         'worldwide': worldwide,
         'india_data': india,
         'world_data': country_data

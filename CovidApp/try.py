d425c258afa74261113b8c56d4a8ddabb231fee5
Ayushman <ayushman11022002@gmail.com>
2021-07-14 17:51:34 +0530

    final commit

--- CovidApp/try.py
+++ CovidApp/try.py
@@ -6,7 +6,7 @@ session = requests.Session()
 session.headers['User-Agent'] = USER_AGENT
 session.headers['Accept-Language'] = LANGUAGE
 session.headers['Content-Language'] = LANGUAGE
-text= session.get('https://www.google.com/search?q=Coronavirus+delhi&biw=1366&bih=625&tbm=nws&sxsrf=ALeKk02EjSFMS4VAFdmugQbCo6PXEPWIYg%3A1626179779235&ei=w4jtYN3cDd_B3LUPxo-GaA&oq=Coronavirus+india&gs_l=psy-ab.3..0i433i131k1j0i433k1l2j0i433i131k1j0i433k1j0i433i131k1j0i433k1j0i433i131k1l2j0i433k1.112257.113775.0.114002.10.9.0.0.0.0.260.1002.0j2j3.5.0....0...1c.1.64.psy-ab..6.4.859...0j0i433i67k1j0i67k1j0i433i131i273k1j0i273k1.0.uaiWtMRTDTI').text
+text= session.get('https://www.google.com/search?q=Coronavirus+India&biw=1366&bih=625&tbm=nws&sxsrf=ALeKk02EjSFMS4VAFdmugQbCo6PXEPWIYg%3A1626179779235&ei=w4jtYN3cDd_B3LUPxo-GaA&oq=Coronavirus+india&gs_l=psy-ab.3..0i433i131k1j0i433k1l2j0i433i131k1j0i433k1j0i433i131k1j0i433k1j0i433i131k1l2j0i433k1.112257.113775.0.114002.10.9.0.0.0.0.260.1002.0j2j3.5.0....0...1c.1.64.psy-ab..6.4.859...0j0i433i67k1j0i67k1j0i433i131i273k1j0i273k1.0.uaiWtMRTDTI').text
 
 
 from bs4 import BeautifulSoup
@@ -30,4 +30,4 @@ for article in articles:
         continue
 
 
-print(news)
+print(soup)
